c159a326cc7c3d501e3510a5305a2e76d4c96a17
Ridge Kennedy <ridgefs@gmail.com>
2013-06-14 00:42:21 +1200

    Recursive Includes
    Allow nesting of route lists arbitrarily deep.

--- src/cowboy_rerouter.erl
+++ src/cowboy_rerouter.erl
@@ -11,11 +11,15 @@
 
 -type route_match() :: iodata().
 -type route() :: {Path::route_match(), Handler::module(), Opts::any()}.
--type routes() :: [route()].
+-type route_include() :: {Path::route_match(), Routes::routes()}.
+-type route_rule() :: route() | route_include().
+-type routes() :: [route_rule()].
 -export_type([routes/0]).
 
 -type dispatch_match() :: {Re::re:mp(), Groups::[atom()]}.
--type dispatch_rule() :: {Path::dispatch_match(), Handler::module(), Opts::any()}.
+-type dispatch() :: {Path::dispatch_match(), Handler::module(), Opts::any()}.
+-type dispatch_include() :: {Path::dispatch_match(), Rules::dispatch_rules()}.
+-type dispatch_rule() :: dispatch() | dispatch_include().
 -opaque dispatch_rules() :: [dispatch_rule()].
 -export_type([dispatch_rules/0]).
 
@@ -25,15 +29,22 @@ compile(Routes) ->
     [compile_route(R) || R <- Routes].
 
 
--spec compile_route(route()) -> dispatch_rule().
+-spec compile_route(route_rule()) -> dispatch_rule().
 compile_route({Path, Handler, Opts}) ->
+    {compile_match(Path), Handler, Opts};
+compile_route({Path, Routes}) ->
+    {compile_match(Path), compile(Routes)}.
+
+
+-spec compile_match(route_match()) -> dispatch_match().
+compile_match(Path) ->
     {ok, Re} = re:compile(Path),
     {ok, GroupRe} = re:compile("\\(\\?P<([^>]+)>"),
     Groups = case re:run(Path, GroupRe, [{capture, all_but_first, binary}, global]) of
         {match, NamedGroups} -> [binary_to_atom(G, utf8) || [G] <- NamedGroups];
         nomatch -> []
     end,
-    {{Re, Groups}, Handler, Opts}.
+    {Re, Groups}.
 
 
 -spec execute(Req, Env) ->
@@ -42,44 +53,46 @@ compile_route({Path, Handler, Opts}) ->
 execute(Req, Env) ->
     {_, Dispatch} = lists:keyfind(dispatch, 1, Env),
     [Host, Path] = cowboy_req:get([host, path], Req),
-    case match(Dispatch, Host, Path) of
-        {ok, Handler, HandlerOpts, Bindings, HostInfo, PathInfo} ->
+    HostInfo = split_host(Host),
+    PathInfo = split_path(Path),
+    case match(Dispatch, Path, []) of
+        {ok, Handler, HandlerOpts, Bindings} ->
             Req2 = cowboy_req:set_bindings(HostInfo, PathInfo, Bindings, Req),
             {ok, Req2, [{handler, Handler}, {handler_opts, HandlerOpts}|Env]};
-        {error, badrequest, path} ->
-            {error, 400, Req};
         {error, notfound, path} ->
             {error, 404, Req}
     end.
 
 
--spec match(dispatch_rules(), Host::binary() | tokens(), Path::binary()) ->
-    {ok, module(), any(), bindings(), tokens(), tokens()}
-    | {error, notfound, path} | {error, badrequest, path}.
+-spec match(dispatch_rules(), Path::binary(), Bindings::bindings()) ->
+    {ok, module(), any(), bindings()} | {error, notfound, path}.
 match([], _, _) ->
-    {error, notfound, host};
-match([{{PathRe, Groups}, Handler, Opts}|Tail], HostInfo, Path) when is_list(HostInfo) ->
+    {error, notfound, path};
+match([{{PathRe, Groups}, Handler, Opts}|Tail], Path, Bindings) ->
     case re:run(Path, PathRe, [{capture, Groups, binary}]) of
         nomatch ->
-            match(Tail, HostInfo, Path);
+            match(Tail, Path, Bindings);
         match ->
-            case split_path(Path) of
-                badrequest ->
-                    {error, badrequest, path};
-                PathInfo ->
-                    {ok, Handler, Opts, [], HostInfo, PathInfo}
-            end;
+            {ok, Handler, Opts, Bindings};
         {match, Captured} ->
-            Bindings = lists:zip(Groups, Captured),
-            case split_path(Path) of
-                badrequest ->
-                    {error, badrequest, path};
-                PathInfo ->
-                    {ok, Handler, Opts, Bindings, HostInfo, PathInfo}
-            end
+            Bindings2 = Bindings ++ lists:zip(Groups, Captured),
+            {ok, Handler, Opts, Bindings2}
     end;
-match(Dispatch, Host, Path) ->
-    match(Dispatch, split_host(Host), Path).
+match([{{PathRe, Groups}, Paths}|Tail], Path, Bindings) ->
+    case re:run(Path, PathRe, [{capture, first}]) of
+        nomatch ->
+            match(Tail, Path, Bindings);
+        {match, [{_, End}]} ->
+            Path2 = binary:part(Path, {End, byte_size(Path)-End}),
+            Bindings2 = case re:run(Path, PathRe, [{capture, Groups, binary}]) of
+                match -> Bindings;
+                {match, Captured} -> Bindings ++ lists:zip(Groups, Captured)
+            end,
+            case match(Paths, Path2, Bindings2) of
+                {error, notfound, path} -> match(Tail, Path2, Bindings2);
+                Match -> Match
+            end
+    end.
 
 
 -spec split_host(binary()) -> tokens().
@@ -99,11 +112,11 @@ split_host(Host, Acc) ->
     end.    
 
 
--spec split_path(binary()) -> tokens() | badrequest.
+-spec split_path(binary()) -> tokens().
 split_path(<< $/, Path/bits >>) ->
     split_path(Path, []);
 split_path(_) ->
-    badrequest.
+    [].
 
 split_path(Path, Acc) ->
     try
@@ -118,7 +131,7 @@ split_path(Path, Acc) ->
         end
     catch
         error:badarg ->
-            badrequest
+            []
     end.
 
 
@@ -133,22 +146,42 @@ compile_test() ->
     [{{_, [a, b, c]}, h, o}] = compile(
         [{"/(?P<a>\\d)/(?P<b>\\d)/(?P<c>\\d)", h, o}]),
     [{{_, []}, h, o}] = compile(
-        [{"/путь/к/ресурсу/", h, o}]).
+        [{"/путь/к/ресурсу/", h, o}]),
+    
+    [{{_, [a, b, c]}, h, o}, {{_, []}, [{{_, []}, h, o}]}] = compile([
+        {"/(?P<a>\\d)/(?P<b>\\d)/(?P<c>\\d)", h, o},
+        {"^/include", [
+            {"^/about$", h, o}
+        ]}
+    ]).
 
 match_test_() ->
     Dispatch = compile([
         {"^/not/here$", match_not, []},
         {"^/users/(?P<user>\\d+)/mails$", match_capture_user, []},
-        {"/(?P<a>\\d)/(?P<b>\\d)/(?P<c>\\d)", match_triplet, []}
+        {"^/(?P<a>\\d)/(?P<b>\\d)/(?P<c>\\d)", match_triplet, []},
+        {"^/secret", [
+            {"^/stuff$", match_secret_stuff, []}
+        ]},
+        {"^/users/(?P<user>\\d+)", [
+            {"^/profile$", match_user_profile, []},
+            {"^/tasks/(?P<task>\\w+)$", match_user_task, []}
+        ]}
     ]),
     Tests = [
         {<<"">>, <<"/users/42/mails">>, 
             {ok, match_capture_user, [], [{user, <<"42">>}]}},
         {<<"">>, <<"/1/2/3">>,
-            {ok, match_triplet, [], [{a, <<"1">>}, {b, <<"2">>}, {c, <<"3">>}]}}
+            {ok, match_triplet, [], [{a, <<"1">>}, {b, <<"2">>}, {c, <<"3">>}]}},
+        {<<"">>, <<"/secret/stuff">>,
+            {ok, match_secret_stuff, [], []}},
+        {<<"">>, <<"/users/42/profile">>,
+            {ok, match_user_profile, [], [{user, <<"42">>}]}},
+        {<<"">>, <<"/users/42/tasks/tidyup">>,
+            {ok, match_user_task, [], [{user, <<"42">>}, {task, <<"tidyup">>}]}}
     ],
     [{lists:flatten(io_lib:format("~p, ~p", [H, P])), fun() ->
-        {ok, Handler, Opts, Binds, _, _} = match(Dispatch, H, P)
-    end} || {H, P, {ok, Handler, Opts, Binds}} <- Tests].
+        Expect = match(Dispatch, P, [])
+    end} || {H, P, Expect} <- Tests].
 
 -endif.
